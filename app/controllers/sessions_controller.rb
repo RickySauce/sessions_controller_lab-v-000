05df227faa4f68be50e38547290e45592ae6e1c4
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-26 17:00:06 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     if params[:name].blank?
       redirect_to login_path
+    else
+      session[:name] = params[:name]
+    end
     binding.pry
   end
 
